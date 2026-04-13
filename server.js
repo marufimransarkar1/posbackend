163b82f85192d6d509a1efc5617b4f9b76aecb6b
unknown <marufimran434@gmail.com>
2026-04-13 21:41:44 +0600

    this is the final commit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
