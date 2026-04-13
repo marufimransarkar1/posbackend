e04188af831c5f6262a87b15234e9d7f03936703
unknown <marufimran434@gmail.com>
2026-04-13 22:18:54 +0600

    this is the final commit i think again

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://pos-inky-two.vercel.app/',
+  origin: process.env.FRONTEND_URL || 'https://pos-inky-two.vercel.app',
   credentials: true
 }));
 
